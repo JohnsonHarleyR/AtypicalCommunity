fd1b863200bc4723e2460b9adb233e09c674bd81
Harley Johnson <hjohnson@uwm.com>
2021-07-01 15:06:42 -0400

    Created ability in orchestrator to transfer money between users and also create bank accounts. If an account doesn't exist, it will be created.

--- Atypical.Domain/Orchestrators/Bank/BankOrchestrator.cs
+++ Atypical.Domain/Orchestrators/Bank/BankOrchestrator.cs
@@ -21,6 +21,96 @@ namespace Atypical.Domain.Orchestrators.Bank
 
 
         /// <summary>
+        /// Get an account based on a user's Id number. If it doesn't exist, create a new account to return.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public BankAccountDto GetAccountByUserId(int userId)
+        {
+            // grab the account
+            BankAccountDto newAccountDto = bankRepository.GetAccountByUserId(userId);
+
+            // if the account is null, create a new account for that user
+            if (newAccountDto == null)
+            {
+                // attempt to create an account for that user
+                bool success = CreateBankAccount(userId);
+
+                // if successful, grab that new bank account and return it
+                if (success)
+                {
+                    BankAccountDto accountDto = bankRepository.GetAccountByUserId(userId);
+                    if (accountDto != null)
+                    {
+                        return accountDto;
+                    }
+                }
+
+                // otherwise, return null
+                return null;
+            }
+
+            // otherwise, return the accountDto
+            return newAccountDto;
+
+        }
+
+
+        // create a new bank account based on a user's id. This will automatically start with a default amount.
+        public bool CreateBankAccount(int userId)
+        {
+            // create account
+            BankAccountDto newAccount = new BankAccountDto()
+            {
+                UserId = userId,
+                Checking = 0,
+                Savings = 100
+            };
+
+            // try to add it to the database - if it doesn't work, that means there is probably already an account for that id
+            try
+            {
+                bankRepository.AddAccount(newAccount);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            
+        }
+
+        public bool TransferBetweenAccounts(double amount, 
+            BankAccountDto sendingAccount, BankAccountDto receivingAccount)
+        {
+            // if eith dto is null, return false
+            if (sendingAccount == null || receivingAccount == null)
+            {
+                return false;
+            }
+
+            // subtract the amount from the dto account
+            sendingAccount.Checking -= amount;
+
+            // if the amount is now less than 0, return false - as it could not be done
+            if (sendingAccount.Checking < 0)
+            {
+                return false;
+            }
+
+            // otherwise, add the amount to Savings
+            receivingAccount.Savings += amount;
+
+            // update the accounts
+            bankRepository.UpdateAccount(sendingAccount);
+            bankRepository.UpdateAccount(receivingAccount);
+
+            // return true
+            return true;
+        }
+
+
+        /// <summary>
         /// Add money to checking account. This is the default for gaining money.
         /// </summary>
         /// <param name="amount"></param>
@@ -154,8 +244,8 @@ namespace Atypical.Domain.Orchestrators.Bank
             // subtract the amount from the dto account
             accountDto.Savings -= amount;
 
-            // if the amount is now less than 0, return false - as it could not be done
-            if (accountDto.Savings < 0)
+            // if the amount is now less than 5, return false - as it could not be done
+            if (accountDto.Savings < 5)
             {
                 return false;
             }
@@ -184,8 +274,8 @@ namespace Atypical.Domain.Orchestrators.Bank
             // subtract the amount from the dto account
             accountDto.Savings -= amount;
 
-            // if the amount is now less than 0, return false - as it could not be done
-            if (accountDto.Savings < 0)
+            // if the amount is now less than 5, return false - as it could not be done
+            if (accountDto.Savings < 5)
             {
                 return false;
             }
